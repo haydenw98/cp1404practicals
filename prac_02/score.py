27bf1d3711e74d258e25b998baf5865e1498a98e
Hayden <hayden.watts98@yahoo.com>
2025-05-31 22:42:36 +1000

    refactor score evaluation program

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,11 +1,19 @@
 
+def main():
+    score = float(input("Enter score: "))
+    print(evaluate_score(score))
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+
+def evaluate_score(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
