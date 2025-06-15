52c7334fe42c98ff3d1858770b4cb46c8c44b3ee
Hayden <hayden.watts98@yahoo.com>
2025-06-15 18:46:04 +1000

    Add lists_warmup.py and complete questions in comment

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""numbers[0] = 3 because it's the first number
+numbers[-1] = 2 since it would wrap around
+numbers[3] = 1 counting from left to right with 3 as 0
+numbers[:-1] = 3, 1, 4, 1, 5, 9, 2 as it would list them through to -1
+numbers[3:4] = 3, 4 as it counts from the third to the fourth
+5 in numbers = true
+7 in numbers = false
+"3" in numbers = false as its not a string
+numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
+
+def main():
+
+main()
