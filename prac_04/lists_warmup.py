53b373123de841af0038c933e13c9e931f041e07
Hayden <hayden.watts98@yahoo.com>
2025-06-15 18:53:58 +1000

    Complete lists_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,5 +11,10 @@ numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
 
 def main():
+    numbers[0] = "ten"
+    numbers[-1] = 1
+    #print(numbers)
+    print(numbers[2:])
+    print(9 in numbers)
 
 main()
