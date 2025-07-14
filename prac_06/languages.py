7308495f284dd4b2bb75c9b05b7ee10e1a6160b9
Hayden <hayden.watts98@yahoo.com>
2025-07-14 21:41:42 +1000

    Add finished languages.py and programming_language.py

---
+++ prac_06/languages.py
@@ -0,0 +1,19 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    # Create ProgrammingLanguage objects, store in a list and print dynamic languages
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+    #print(languages)
+    #print(python)
+
+main()
